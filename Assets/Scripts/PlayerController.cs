2d264ce2c6c60458c10aa66678648ce26d0c7bd6
HarunOYusuf <harunosmanyusuf@gmail.com>
2024-11-26 01:35:41 +0000

    basic Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float walkSpeed = 3f;
+    private Vector2 moveInput;
+    public bool IsMoving { get; set; }
+    
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        
+        IsMoving = moveInput != Vector2.zero;
+    }
+
     
 }
