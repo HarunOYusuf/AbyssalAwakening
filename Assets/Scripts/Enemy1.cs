ecf8bfde5aab02ec9fe90f09232e0dda74195dd6
HarunOYusuf <harunosmanyusuf@gmail.com>
2024-12-18 16:55:36 +0000

    Wed

---
+++ Assets/Scripts/Enemy1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 2f;
+    private bool movingToB = true;
+    public float threshold = 0.1f;
+    
+    private void Update()
+    {
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        Vector2 targetPosition = movingToB ? pointB.position : pointA.position;
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, targetPosition) <= threshold)
+        {
+            transform.position = targetPosition;
+            movingToB = !movingToB;
+        }
+    }
+}
